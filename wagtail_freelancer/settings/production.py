9ca0534836b8c664cc0e10ca8bef9b4a34c96544
MichaelYin <hgyinjian@gmail.com>
2018-09-08 12:12:45 +0800

    add env in production.py

--- wagtail_freelancer/settings/production.py
+++ wagtail_freelancer/settings/production.py
@@ -1,6 +1,13 @@
 from .base import *
+import os
+
+env = os.environ.copy()
 
 DEBUG = False
+
+SECRET_KEY = env['SECRET_KEY']
+
+ALLOWED_HOSTS = env['ALLOWED_HOSTS'].split(',')
 
 try:
     from .local import *
