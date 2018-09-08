dbbefdb37ada78d14a3be158bac5d3d7676ecf53
MichaelYin <hgyinjian@gmail.com>
2018-09-08 12:07:00 +0800

    update model path

--- wagtail_freelancer/urls.py
+++ wagtail_freelancer/urls.py
@@ -6,7 +6,6 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
-
 urlpatterns = [
     url(r'^django-admin/', admin.site.urls),
 
